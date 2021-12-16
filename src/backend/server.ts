4f62dc5dea559635533158e2611e4448a6056ecd
minami kotaro <minami-kotaro@ksc.co.jp>
2021-12-16 10:38:25 +0900

    クライアント側(仮)

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import helmet from "helmet";
-import cors from "cors";
 import router from "./router";
 import path from "path";
 
 const app = express();
 app.use(helmet());
-app.use(cors());
 app.use(
   express.urlencoded({
     extended: true,
--- src/frontend/views/About.vue
+++ src/frontend/views/About.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Text</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in memos" :key="row.id">
+          <td>{{ row.id }}</td>
+          <td>{{ row.title }}</td>
+          <td>{{ row.text }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import axios from "axios";
+@Component({
+  name: "app",
+})
+export default class extends Vue {
+  memos: { id: number; title: string; text: string }[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async created() {
+    const res = await axios.get("http://localhost:3000/api/version");
+    this.memos = res.data.map(
+      (x: { id: number; title: string; text: string }) => ({
+        id: x.id,
+        title: x.title,
+        text: x.text,
+      })
+    );
+  }
+}
+</script>
